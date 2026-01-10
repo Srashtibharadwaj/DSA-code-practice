2a37d702f1e1d970df2920cb32aff014446ca84f
Srashtibharadwaj <srashti1102005@gmail.com>
2026-01-10 15:43:03 +0530

    Added Java sorting algorithms

---
+++ merge_sort.java
@@ -0,0 +1,69 @@
+public class merge_sort {
+
+    
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int mid = l + (r - l) / 2;
+
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+
+            merge(arr, l, mid, r);
+        }
+    }
+
+
+    public static void merge(int[] arr, int l, int mid, int r) {
+
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
